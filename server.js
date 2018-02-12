8cc0e6701b5edea1198e53bd62a3f4983d106063
Vernon Mensah <vernonmensah@gmail.com>
2018-02-12 18:55:23 -0500

    imported list router

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ mongoose.Promise = global.Promise;
 const userRouter = require('./routers/userRouter');
 const campaignRouter = require('./routers/campaignRouter');
 const eventDataRouter = require('./routers/eventDataRouter');
+const listRouter = require('./routers/listRouter');
 const app = express();
 
 app.use(morgan('common'));
@@ -18,9 +19,10 @@ app.use(bodyParser.json());
 app.use('/user', userRouter);
 app.use('/campaign', campaignRouter);
 app.use('/eventData', eventDataRouter);
+app.use('/list', listRouter);
 
 const DATABASE_URL = process.env.DATABASE_URL;
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT;
 
 app.get('/', function (req, res) {
     res.send('Home!');
