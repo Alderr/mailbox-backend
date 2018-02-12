bde083a0848ab9636b9b97572c91cfc702c5f161
Vernon Mensah <vernonmensah@gmail.com>
2018-02-12 11:41:12 -0500

    dont need count, theres a query for that :tongue:

--- models/eventDataModel.js
+++ models/eventDataModel.js
@@ -5,7 +5,6 @@ mongoose.Promise = global.Promise;
 const CampaignEventDataSchema = mongoose.Schema({
     send:
       {
-          count: { type: Number, default: 0 },
           emails: [
               { type: String, required: true }
           ]
@@ -13,7 +12,6 @@ const CampaignEventDataSchema = mongoose.Schema({
     ,
     open:
       {
-          count: { type: Number, default: 0 },
           emails: [
               { type: String, required: true }
           ]
@@ -21,7 +19,6 @@ const CampaignEventDataSchema = mongoose.Schema({
     ,
     click:
       {
-          count: { type: Number, default: 0 },
           emails: [
               { type: String, required: true }
           ]
