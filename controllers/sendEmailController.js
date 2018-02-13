cad601ebdbead46d7a880bf2d2a636d7d4f4ef1e
Vernon Mensah <vernonmensah@gmail.com>
2018-02-13 18:37:56 -0500

    added call to SES_SERVER

--- controllers/sendEmailController.js
+++ controllers/sendEmailController.js
@@ -6,12 +6,14 @@ const sendEmailNow = (userId, campaign) => {
 
     //gist: creating a campaign obj to send of to AWS_SES_SERVER
 
-    const AWS_SES_SERVER_URL = process.env.AWS_SES_SERVER_URL;
+    const AWS_SES_SERVER_URL_NOW = process.env.AWS_SES_SERVER_URL_NOW;
     console.log('AWS_SES_SERVER_URL', AWS_SES_SERVER_URL);
     //console.log('CAMPAIGN', JSON.stringify(campaign, null, 2));
 
     console.log('LISTS', campaign.lists);
     console.log('---------------LISTS---------------');
+
+
     //get specific list(s) mentioned in campaign;
     Promise.all(campaign.lists.map(aList => {
         return getList(aList.id, userId);
@@ -21,17 +23,27 @@ const sendEmailNow = (userId, campaign) => {
             console.log('------------------DATA AKA LISTS-----------------S');
             let allLists = [];
 
+            //combine all lists together in one arr
             for(let list in data) {
                 allLists = allLists.concat(data[list].contacts);
             }
 
-            //attach the list to object
+            //attach allList to object aka campaign
             campaign.lists = allLists;
-            console.log('CAMPAIGN.LISTS', JSON.stringify(campaign.lists, null, 2))
+            console.log('CAMPAIGN.LISTS', JSON.stringify(campaign.lists, null, 2));
             console.log('-------------------CAMPAIGN.LISTS---------------');
 
-            //send to AWS_SES_SERVER
-            
+            //send the campaign to AWS_SES_SERVER
+            //Send a POST request
+            axios({
+                method: 'post',
+                url: AWS_SES_SERVER_URL_NOW,
+                data: campaign
+            })
+                .then(data => {
+                    console.log(data);
+                    console.log('---------------Successful!------------');
+                });
         })
         .catch(err => {
             console.log(err);
@@ -44,3 +56,4 @@ const sendEmailNow = (userId, campaign) => {
 
 
 module.exports = { sendEmailNow };
+{ sendEmailNow; }
