d864cb23e706575d48e09d35e3068727665b69f2
Vernon Mensah <vernonmensah@gmail.com>
2018-02-13 23:11:11 -0500

    Changed eventDataModel to match aws-sns

--- models/eventDataModel.js
+++ models/eventDataModel.js
@@ -3,7 +3,7 @@ mongoose.Promise = global.Promise;
 
 
 const CampaignEventDataSchema = mongoose.Schema({
-    send:
+    delivery:
       {
           emails: [
               { type: String, required: true }
@@ -20,10 +20,26 @@ const CampaignEventDataSchema = mongoose.Schema({
     click:
       {
           emails: [
-              { type: String, required: true }
+              {
+                email: { type: String, required: true },
+                clickEvents: [
+                  {
+                    timestamp: { type: String, required: true },
+                    link: { type: String, required: true }
+                  }
+                ]
+              }
           ]
       }
-
+/* click: {
+      emails: [
+      email:
+      clickEvents: {
+      date:
+      link:
+    }
+    ]
+} */
 });
 
 
